f7e8b3d2e111c5b61f7862942441ad44e22a73b8
Matt Beman <mbeman@gmail.com>
2020-08-17 15:58:48 -0700

    Add project files.

---
+++ GardenTracker/Model/GardenSquare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenTracker.Model
+{
+    class GardenSquare
+    {
+        public Plant PlantType { get; }
+
+        public int XCoordinate { get; }
+
+        public int YCoordinate { get; }
+    }
+}
---
+++ GardenTracker/Model/Plant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenTracker.Model
+{
+    public class Plant
+    {
+        public Plant(int gestation, DateTime plantDate, string name, int seeds, string notes)
+        {
+            Gestation = gestation;
+            PlantDate = plantDate;
+            Name = name;
+            SeedsPerSquare = seeds;
+            Notes = notes;
+        }
+
+        public int Gestation { get; }
+        
+        public string Name { get; }
+
+        public string Notes { get; set; }
+
+        public int SeedsPerSquare { get; }
+
+        public DateTime PlantDate { get; }
+    }
+}
